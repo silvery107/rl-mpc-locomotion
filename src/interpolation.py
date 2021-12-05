c65aece35c8b26ced22f8452238f475d6623ae63
SilverSoul <1079931505@qq.com>
2021-12-05 16:19:15 +0800

    update MIT

---
+++ src/interpolation.py
@@ -0,0 +1,29 @@
+# Utility functions to interpolate between two values
+# y0 (3,1)
+# yf (3,1)
+# x float
+
+
+#  Cubic bezier interpolation between y0 and yf.  x is between 0 and 1
+def cubicBezier(y0, yf, x:float):
+    """x must use floating point value"""
+    assert(x >= 0 and x <= 1)
+    yDiff = yf - y0
+    bezier = x * x * x + float(3) * (x * x * (float(1) - x))
+    return y0 + bezier * yDiff
+
+# Cubic bezier interpolation derivative between y0 and yf.  x is between 0 and 1
+def cubicBezierFirstDerivative(y0, yf, x:float):
+    """x must use floating point value"""
+    assert(x >= 0 and x <= 1)
+    yDiff = yf - y0
+    bezier = float(6) * x * (float(1) - x)
+    return bezier * yDiff
+
+# Cubic bezier interpolation derivative between y0 and yf.  x is between 0 and 1
+def cubicBezierSecondDerivative(y0, yf, x):
+    """x must use floating point value"""
+    assert(x >= 0 and x <= 1)
+    yDiff = yf - y0
+    bezier = float(6) - float(12) * x
+    return bezier * yDiff
---
+++ src/robot_state.py
@@ -0,0 +1,49 @@
+import numpy as np
+import quaternion
+
+class RobotState:
+
+    def __init__(self) -> None:
+        self.p = np.zeros((3, 1))
+        self.v = np.zeros((3, 1))
+        self.w = np.zeros((3, 1))
+        self.r_feet = np.zeros((3, 4))
+        self.R = np.zeros((3, 3))
+        self.R_yaw = np.zeros((3, 3))
+        # self.I_body = np.zeros((3, 3))
+        self.q = np.quaternion(0, 0, 0, 0)
+        self.yaw = 0.0
+        self.m = 9.0
+
+    def set(self, p_, v_, q_, w_, r_, yaw_):
+        
+        self.p = np.copy(p_)
+        self.v = np.copy(v_)
+        self.w = np.copy(w_)
+        self.q.w = q_[0]
+        self.q.x = q_[1]
+        self.q.y = q_[2]
+        self.q.z = q_[3]
+        self.yaw = yaw_
+        for rs in range(4):
+            for c in range(5):
+                self.r_feet[rs, c] = r_[rs*4+c]
+
+        self.R = quaternion.as_rotation_matrix(self.q)
+        yc = np.cos(yaw_)
+        ys = np.sin(yaw_)
+        self.R_yaw = np.array([[yc,  -ys,   0],
+                               [ys,  yc,   0],
+                               [0,   0,   1]])
+        # np.fill_diagonal(self.I_body, [0.07, 0.26, 0.242])
+
+
+    def printState(self):
+        print("Robot State:\n" +
+            "Position\n" + self.p.T +
+            "\nVelocity\n" + self.v.T +
+            "\nAngular Velocity\n" + self.w.T +
+            "\nRotation\n" + self.R + 
+            "\nYaw Rotation\n" + self.R_yaw +
+            "\nFoot Locations\n"+ self.r_feet +
+            "\nInertia\n" + self.I_body + "\n")
