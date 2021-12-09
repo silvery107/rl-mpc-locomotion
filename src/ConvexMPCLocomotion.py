8a27e803ee8ef1a98fe09390c9f0656f65203a72
SilverSoul <1079931505@qq.com>
2021-12-09 22:20:52 +0800

    update

---
+++ src/ConvexMPCLocomotion.py
@@ -0,0 +1,14 @@
+import numpy as np
+import LegController as legctl
+
+DTYPE = np.float32
+
+class CMPC_Result:
+    commands = [legctl.LegController() for _ in range(4)]
+    contactPhase = np.zeros((4,1), dtype=DTYPE)
+
+
+class ConvexMPCLocomotion:
+    # TODO finish this fucking class
+    pass
+    
