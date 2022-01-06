dc272a7dd81b5238b8adb34f4e17157933caab7b
SilverSoul <1079931505@qq.com>
2022-01-07 00:02:21 +0800

    add desiredStateCommand

---
+++ MPC_Controller/DesiredStateCommand.py
@@ -0,0 +1,12 @@
+
+
+class DesiredStateCommand:
+    def __init__(self) -> None:
+        self.yaw_turn_rate = 0.0
+        self.x_vel_cmd = 0.0
+        self.y_vel_cmd = 0.0
+
+    def update(self, command):
+        self.yaw_turn_rate = command.yaw_turn_rate
+        self.x_vel_cmd = command.x_vel_cmd
+        self.y_vel_cmd = command.y_vel_cmd
