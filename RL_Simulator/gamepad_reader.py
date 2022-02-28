8348cab6d98f3a6b5af7f56c2e84523a01f27643
SilverSoul <1079931505@qq.com>
2022-02-28 16:46:16 +0800

    add gamepad detect

--- RL_Simulator/gamepad_reader.py
+++ RL_Simulator/gamepad_reader.py
@@ -1,12 +1,12 @@
 from absl import app
 from absl import flags
+import inputs
 from inputs import get_gamepad
 import threading
 import time
 
 FLAGS = flags.FLAGS
-MAX_ABS_RX = 32768
-MAX_ABS_RY = 32768
+MAX_ABS_VAL = 32768
 
 
 def _interpolate(raw_reading, max_raw_reading, new_scale):
@@ -14,23 +14,32 @@ def _interpolate(raw_reading, max_raw_reading, new_scale):
 
 
 class Gamepad:
-  """Interface for reading commands from Logitech F710 Gamepad.
+  """Interface for reading commands from xbox Gamepad.
 
   The control works as following:
   1) Press LB+RB at any time for emergency stop
   2) Use the left joystick for forward/backward/left/right walking.
   3) Use the right joystick for rotation around the z-axis.
   """
-  def __init__(self, vel_scale_x=.4, vel_scale_y=.4, vel_scale_rot=1.):
+  def __init__(self, 
+               vel_scale_x: float=.5, 
+               vel_scale_y: float=.5, 
+               vel_scale_rot: float=1.,
+               max_acc: float = .5):
     """Initialize the gamepad controller.
     Args:
       vel_scale_x: maximum absolute x-velocity command.
       vel_scale_y: maximum absolute y-velocity command.
       vel_scale_rot: maximum absolute yaw-dot command.
     """
+    if not inputs.devices.gamepads:
+      raise "Please plug in your xbox gamepad!"
+
+    self.gamepad = inputs.devices.gamepads[0]
     self._vel_scale_x = vel_scale_x
     self._vel_scale_y = vel_scale_y
     self._vel_scale_rot = vel_scale_rot
+    self._max_acc = max_acc
     self._lb_pressed = False
     self._rb_pressed = False
 
@@ -48,10 +57,13 @@ class Gamepad:
     This funnction should be executed in a separate thread for continuous
     event recording.
     """
-    while self.is_running and not self.estop_flagged:
-      events = get_gamepad()
-      for event in events:
-        self.update_command(event)
+    while self.is_running:# and not self.estop_flagged:
+      try:
+        events = self.gamepad.read()
+        for event in events:
+          self.update_command(event)
+      except Exception as e:
+        pass
 
   def update_command(self, event):
     """Update command based on event readings."""
@@ -59,21 +71,28 @@ class Gamepad:
       self._lb_pressed = bool(event.state)
     elif event.ev_type == 'Key' and event.code == 'BTN_TR':
       self._rb_pressed = bool(event.state)
+    elif event.ev_type == 'Key' and event.code == 'BTN_THUMBL':
+      self._lj_pressed = bool(event.state)
     elif event.ev_type == 'Absolute' and event.code == 'ABS_X':
       # Left Joystick L/R axis
-      self.vy = _interpolate(-event.state, MAX_ABS_RX, self._vel_scale_y)
+      self.vy = _interpolate(-event.state, MAX_ABS_VAL, self._vel_scale_y)
     elif event.ev_type == 'Absolute' and event.code == 'ABS_Y':
       # Left Joystick F/B axis; need to flip sign for consistency
-      self.vx = _interpolate(-event.state, MAX_ABS_RY, self._vel_scale_x)
+      self.vx = _interpolate(-event.state, MAX_ABS_VAL, self._vel_scale_x)
     elif event.ev_type == 'Absolute' and event.code == 'ABS_RX':
-      self.wz = _interpolate(event.state, MAX_ABS_RX, self._vel_scale_rot)
+      self.wz = _interpolate(-event.state, MAX_ABS_VAL, self._vel_scale_rot)
+    
+    if self.estop_flagged and self._lj_pressed:
+      self.estop_flagged = False
+      print("Estop Released.")
 
     if self._lb_pressed and self._rb_pressed:
+      print("EStop Flagged, press LEFT joystick to release.")
       self.estop_flagged = True
       self.vx, self.vy, self.wz = 0., 0., 0.
 
-  def get_command(self, time_since_reset):
-    del time_since_reset  # unused
+  def get_command(self):
+    
     return (self.vx, self.vy, 0), self.wz, self.estop_flagged
 
   def stop(self):
