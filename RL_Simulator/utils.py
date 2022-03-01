08df5dc9ae0c00468605eae2b2173a98c726faf9
SilverSoul <1079931505@qq.com>
2022-03-01 21:41:16 +0800

    update sim params

--- RL_Simulator/utils.py
+++ RL_Simulator/utils.py
@@ -18,17 +18,24 @@ def acquire_sim(gym, dt):
 
     # set common parameters
     sim_params.dt = dt # control timestep
-    sim_params.substeps = 1 # physics simulation timestep
+    sim_params.substeps = 2 # physics simulation timestep
     sim_params.up_axis = gymapi.UP_AXIS_Z
-    sim_params.gravity = gymapi.Vec3(0.0, 0.0, -9.8)
+    sim_params.gravity = gymapi.Vec3(0.0, 0.0, -9.81)
 
     # set PhysX-specific parameters
     sim_params.physx.use_gpu = True
     sim_params.physx.solver_type = 1
-    sim_params.physx.num_position_iterations = 6
+    sim_params.physx.num_position_iterations = 4
     sim_params.physx.num_velocity_iterations = 1
-    sim_params.physx.contact_offset = 0.01
+    sim_params.physx.contact_offset = 0.02
     sim_params.physx.rest_offset = 0.0
+    sim_params.physx.bounce_threshold_velocity = 0.2
+    sim_params.physx.max_depenetration_velocity = 100.0
+    sim_params.physx.default_buffer_size_multiplier = 5.0
+    sim_params.physx.max_gpu_contact_pairs = 8388608 # 8*1024*1024
+
+
+    # 0: CC_NEVER (don't collect contact info), 1: CC_LAST_SUBSTEP (collect only contacts on last substep), 2: CC_ALL_SUBSTEPS (default - all contacts)
 
     # create sim with these parameters
     sim = gym.create_sim(compute_device=0, graphics_device=0, type=gymapi.SIM_PHYSX, params=sim_params)
@@ -36,7 +43,7 @@ def acquire_sim(gym, dt):
     # configure the ground plane
     plane_params = gymapi.PlaneParams()
     plane_params.normal = gymapi.Vec3(0, 0, 1)  # z-up!
-    plane_params.distance = 0
+    # plane_params.distance = 0
     plane_params.static_friction = 1
     plane_params.dynamic_friction = 1
     plane_params.restitution = 0    # control the elasticity of collisions (amount of bounce)
@@ -60,6 +67,11 @@ def load_asset(gym, sim, robot, fix_base_link):
     asset_options.fix_base_link = fix_base_link
     asset_options.use_mesh_materials = True
     asset_options.flip_visual_attachments = False if asset_file==XIAOTIAN or asset_file==MINI_CHEETAH or asset_file==A1 else True
+    asset_options.density = 0.001
+    asset_options.angular_damping = 0.0
+    asset_options.linear_damping = 0.0
+    asset_options.thickness = 0.01
+    asset_options.disable_gravity = False
     asset_options.armature = 0.01   # added to the diagonal elements of inertia tensors
                                     # for all of the asset’s rigid bodies/links. 
                                     # Could improve simulation stability
