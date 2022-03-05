def836d42ea313ca96647347001f4ec40aa87143
SilverSoul <1079931505@qq.com>
2022-03-05 17:06:25 +0800

    remove unsed trajectory design

--- MPC_Controller/Parameters.py
+++ MPC_Controller/Parameters.py
@@ -4,10 +4,11 @@ from MPC_Controller.utils import GaitType, FSM_OperatingMode, FSM_StateName
 class Parameters:
     cmpc_x_drag = 3.0
     cmpc_bonus_swing = 0.0
-    cmpc_alpha = 4e-5
+    cmpc_alpha = 1e-5
     
     cmpc_print_solver_time = False
     cmpc_print_total_time = False
+    cmpc_print_states = False
 
     cmpc_py_solver = 1 # 0 cvxopt, 1 osqp
     cmpc_solver_type = 2 # 0 mit py solver, 1 mit cpp solver, 2 google cpp solver
