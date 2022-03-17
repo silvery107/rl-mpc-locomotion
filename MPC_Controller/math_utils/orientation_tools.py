25347e71af264cbde01bea5b6997ff4350f027bd
SilverSoul <1079931505@qq.com>
2022-03-17 18:42:34 +0800

    fix data type

--- MPC_Controller/math_utils/orientation_tools.py
+++ MPC_Controller/math_utils/orientation_tools.py
@@ -19,10 +19,10 @@ def axis_angle_to_rot(k, theta):
     v_t = 1 - c_t
 
     R_axis_angle = np.array([
-        k[0]*k[0]*v_t + c_t, k[0]*k[1]*v_t - k[2]*s_t, k[0]*k[2]*v_t + k[1]*s_t,
-        k[0]*k[1]*v_t + k[2]*s_t, k[1]*k[1]*v_t + c_t, k[1]*k[2]*v_t - k[0]*s_t,
-        k[0]*k[2]*v_t - k[1]*s_t, k[1]*k[2]*v_t + k[0]*s_t, k[1]*k[1]*v_t + c_t
-        ]).reshape((3,3))
+        k[0]*k[0]*v_t + c_t,        k[0]*k[1]*v_t - k[2]*s_t,   k[0]*k[2]*v_t + k[1]*s_t,
+        k[0]*k[1]*v_t + k[2]*s_t,   k[1]*k[1]*v_t + c_t,        k[1]*k[2]*v_t - k[0]*s_t,
+        k[0]*k[2]*v_t - k[1]*s_t,   k[1]*k[2]*v_t + k[0]*s_t,   k[1]*k[1]*v_t + c_t
+        ], dtype=DTYPE).reshape((3,3))
     
     return R_axis_angle.T
 
