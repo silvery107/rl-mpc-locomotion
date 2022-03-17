8ff5112a3eaecced2808773ec658b1a31b2d3884
SilverSoul <1079931505@qq.com>
2022-03-17 19:31:53 +0800

    fix ground normal estimate bug

--- MPC_Controller/StateEstimator.py
+++ MPC_Controller/StateEstimator.py
@@ -3,7 +3,7 @@ from MPC_Controller.Parameters import Parameters
 from MPC_Controller.common.Quadruped import Quadruped
 from MPC_Controller.math_utils.moving_window_filter import MovingWindowFilter
 from MPC_Controller.utils import Quaternion, DTYPE
-from MPC_Controller.math_utils.orientation_tools import quat_to_rot, quat_to_rpy,rot_to_rpy, get_rot_from_normals
+from MPC_Controller.math_utils.orientation_tools import quat_to_rot, quat_to_rpy,rot_to_rpy, get_rot_from_normals, rpy_to_rot
 
 class StateEstimate:
     def __init__(self):
@@ -16,7 +16,8 @@ class StateEstimate:
         self.rpy = np.zeros((3,1), dtype=DTYPE)
         self.rpyBody = np.zeros((3,1), dtype=DTYPE)
 
-        self.ground_normal = np.array([0,0,1], dtype=DTYPE)
+        self.ground_normal_world = np.array([0,0,1], dtype=DTYPE)
+        self.ground_normal_yaw = np.array([0,0,1], dtype=DTYPE)
 
         self.vBody = np.zeros((3,1), dtype=DTYPE)
         self.omegaBody = np.zeros((3,1), dtype=DTYPE)
@@ -26,13 +27,23 @@ class StateEstimator:
     def __init__(self, quadruped:Quadruped):
         self.result = StateEstimate()
         self._quadruped = quadruped
+        # self._ground_normal_filter = MovingWindowFilter(window_size=10)
+        # self._velocity_filter = MovingWindowFilter(window_size=60)
         self._phase = np.zeros((4,1), dtype=DTYPE)
-        self._ground_normal_filter = MovingWindowFilter(window_size=10)
         self._contactPhase = self._phase
         self._foot_contact_history:np.ndarray = None
 
         self.ground_R_body_frame:np.ndarray = None
         self.body_height:float = self._quadruped._bodyHeight
+        self.result.position[2] = self.body_height
+
+    def reset(self):
+        self.result = StateEstimate()
+        self._phase = np.zeros((4,1), dtype=DTYPE)
+        self._phase = np.zeros((4,1), dtype=DTYPE)
+        self._contactPhase = self._phase
+        self._foot_contact_history:np.ndarray = None
+        self.ground_R_body_frame:np.ndarray = None
         self.result.position[2] = self.body_height
 
     def setContactPhase(self, phase:np.ndarray):
@@ -73,10 +84,12 @@ class StateEstimator:
         #  RPY of body in the world frame
         self.result.rpy = quat_to_rpy(self.result.orientation)
 
-        # world_R_ground_frame = rpy_to_rot([0,0,self.result.rpy[2]])
-        world_R_ground_frame = get_rot_from_normals(np.array([0,0,1], dtype=DTYPE),
-                                                    self.result.ground_normal)
-        self.ground_R_body_frame = self.result.rBody @ world_R_ground_frame.T
+        world_R_yaw_frame = rpy_to_rot([0,0,self.result.rpy[2]])
+        yaw_R_ground_frame = get_rot_from_normals(np.array([0,0,1], dtype=DTYPE),
+                                                    self.result.ground_normal_yaw)
+ 
+        # self.ground_R_body_frame = self.result.rBody @ yaw_R_ground_frame.T
+        self.ground_R_body_frame = self.result.rBody @ world_R_yaw_frame.T @ yaw_R_ground_frame.T
 
         # RPY of body in yaw aligned ground frame
         self.result.rpyBody = rot_to_rpy(self.ground_R_body_frame)
@@ -86,19 +99,19 @@ class StateEstimator:
         self._foot_contact_history[:, 2] = - self.body_height
 
     def _update_contact_history(self, foot_positions:np.ndarray):
-        foot_contacts = self._contactPhase.flatten().copy()
         foot_positions_ = foot_positions.copy()
         for leg_id in range(4):
-            if foot_contacts[leg_id]:
+            if self._contactPhase[leg_id]:
                 self._foot_contact_history[leg_id] = foot_positions_[leg_id]
 
     def _update_com_position_ground_frame(self, foot_positions:np.ndarray):
-        foot_contacts = self._contactPhase.flatten().copy()
+        foot_contacts = self._contactPhase.flatten()
         if np.sum(foot_contacts) == 0:
             return np.array((0, 0, self.body_height))
         else:
             foot_positions_ground_frame = (foot_positions.reshape((4,3)).dot(self.ground_R_body_frame.T))
             foot_heights = -foot_positions_ground_frame[:, 2]
+
         height_in_ground_frame = np.sum(foot_heights * foot_contacts) / np.sum(foot_contacts)
         self.result.position[2] = height_in_ground_frame
         
@@ -112,11 +125,15 @@ class StateEstimator:
         self._update_contact_history(foot_positions)
 
         contact_foot_positions = self._foot_contact_history.reshape((4,3)) # reshape from (4,3,1) to (4,3)
-        normal_vec = np.linalg.lstsq(contact_foot_positions, np.ones(4), rcond=None)[0]
+        normal_vec = np.linalg.lstsq(contact_foot_positions, np.ones(4, dtype=DTYPE), rcond=None)[0]
         normal_vec /= np.linalg.norm(normal_vec)
         if normal_vec[2] < 0:
             normal_vec = -normal_vec
 
-        _ground_normal = self._ground_normal_filter.calculate_average(normal_vec)
+        # _ground_normal = self._ground_normal_filter.calculate_average(normal_vec)
+        _ground_normal = normal_vec
         _ground_normal /= np.linalg.norm(_ground_normal)
-        self.result.ground_normal = _ground_normal
+
+        # ground normal in yaw aligned ground frame and world frame
+        self.result.ground_normal_yaw = _ground_normal
+        self.result.ground_normal_world = self.result.rBody.T @ _ground_normal
