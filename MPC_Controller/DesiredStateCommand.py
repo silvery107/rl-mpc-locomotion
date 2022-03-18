9f1db9f11ee42fbeeef93130021ad4586657cf73
SilverSoul <1079931505@qq.com>
2022-03-19 00:37:37 +0800

    adapt to weight policy

--- MPC_Controller/DesiredStateCommand.py
+++ MPC_Controller/DesiredStateCommand.py
@@ -9,11 +9,19 @@ class DesiredStateCommand:
         self.yaw_turn_rate = 0.0
         self.mpc_weights = None
 
-    def updateCommand(self, commands):
+    def updateCommand(self, commands, _weight=None):
         self.x_vel_cmd = commands[0]
         self.y_vel_cmd = commands[1]
         self.yaw_turn_rate = commands[2]
-        if len(commands)>3:
+        
+        if _weight is not None:
+            weights = list(_weight)
+            weights.append(0.0)
+            assert len(weights) == 13
+            assert all(w>=0 for w in weights)
+            self.mpc_weights = weights
+
+        elif len(commands)>3:
             weights = list(commands[3:])
             assert len(weights) == 13
             assert all(w>=0 for w in weights)
