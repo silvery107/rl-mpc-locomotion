e81a5e13f60ecc1f0a063bccffaf52c489774720
silvery107 <z1079931505@gmail.com>
2024-02-29 22:52:26 -0500

    add training support of A1 robot

--- RL_Environment/tasks/__init__.py
+++ RL_Environment/tasks/__init__.py
@@ -1,10 +1,12 @@
 from .aliengo import Aliengo
 from .go1 import Go1
+from .a1 import A1Task
 from .legged_config_ppo import LeggedCfgPPO
 
 # Mappings from strings to environments
 isaacgym_task_map = {
     "Aliengo": Aliengo,
     "Go1": Go1,
+    "A1": A1Task,
     "ConfigPPO": LeggedCfgPPO,
 }
